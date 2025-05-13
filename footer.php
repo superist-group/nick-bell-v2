95fa92baa7a9b3ea33c234b024e7d74e9177737e
Jonathan Vu <jonathan.vu@firstpage.com.au>
2025-05-13 14:30:00 +1000

    added hs script

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -11,71 +12,77 @@
 
 ?>
 
-	<footer class="site-footer bg-navy text-white">
-		<div class="tb_container tb_container--sm">
-			<div class="pt-[50px] md:pt-[64px] pb-[50px] flex flex-col md:flex-row justify-between w-full">
-					<div class="w-full md:max-w-[500px] lg:max-w-[600px]">
-						<div class="footer-logo">
-							<a href="<?php echo get_site_url();?>">
-								<img src="<?php echo get_field('footer_logo', 'option'); ?>" alt="<?php echo get_bloginfo('name'); ?>">
-							</a>
-						</div>
-						<?php
-							wp_nav_menu([
-									'theme_location' => 'menu-2',
-									'menu_id'        => 'footer-menu',
-									'container'      => false,
-									'menu_class'     => 'footer-menu', 
-							]);
-						?>
-						<p><?php echo get_field('footer_description', 'option'); ?></p>
-						
-					</div>
-					<div class="w-full md:w-auto mt-[42px] md:mt-[31px] ">
-						<div class="social-link flex gap-4 md:justify-center mb-[24px] md:mb-[40px]">
-							<?php if ($instagram = get_field('social_media_instagram_link', 'option')) : ?>
-								<a href="<?php echo esc_url($instagram); ?>" target="_blank" rel="noopener">
-									<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/social-icon-1.svg" alt="Instagram">
-								</a>
-							<?php endif; ?>
+<footer class="site-footer bg-navy text-white">
+  <div class="tb_container tb_container--sm">
+    <div class="pt-[50px] md:pt-[64px] pb-[50px] flex flex-col md:flex-row justify-between w-full">
+      <div class="w-full md:max-w-[500px] lg:max-w-[600px]">
+        <div class="footer-logo">
+          <a href="<?php echo get_site_url(); ?>">
+            <img src="<?php echo get_field('footer_logo', 'option'); ?>" alt="<?php echo get_bloginfo('name'); ?>">
+          </a>
+        </div>
+        <?php
+				wp_nav_menu([
+					'theme_location' => 'menu-2',
+					'menu_id'        => 'footer-menu',
+					'container'      => false,
+					'menu_class'     => 'footer-menu',
+				]);
+				?>
+        <p><?php echo get_field('footer_description', 'option'); ?></p>
 
-							<?php if ($linkedin = get_field('social_media_linkedin_link', 'option')) : ?>
-								<a href="<?php echo esc_url($linkedin); ?>" target="_blank" rel="noopener">
-									<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/social-icon-2.svg" alt="LinkedIn">
-								</a>
-							<?php endif; ?>
+      </div>
+      <div class="w-full md:w-auto mt-[42px] md:mt-[31px] ">
+        <div class="social-link flex gap-4 md:justify-center mb-[24px] md:mb-[40px]">
+          <?php if ($instagram = get_field('social_media_instagram_link', 'option')) : ?>
+          <a href="<?php echo esc_url($instagram); ?>" target="_blank" rel="noopener">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/social-icon-1.svg" alt="Instagram">
+          </a>
+          <?php endif; ?>
 
-							<?php if ($tiktok = get_field('social_media_tiktok_link', 'option')) : ?>
-								<a href="<?php echo esc_url($tiktok); ?>" target="_blank" rel="noopener">
-									<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/social-icon-3.svg" alt="TikTok">
-								</a>
-							<?php endif; ?>
-						</div>
+          <?php if ($linkedin = get_field('social_media_linkedin_link', 'option')) : ?>
+          <a href="<?php echo esc_url($linkedin); ?>" target="_blank" rel="noopener">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/social-icon-2.svg" alt="LinkedIn">
+          </a>
+          <?php endif; ?>
 
-						<div class="platform-links">
-							<?php if ($youtube = get_field('social_media_youtube', 'option')) : ?>
-								<a class="social-platform-icon" href="<?php echo esc_url($youtube); ?>" target="_blank" rel="noopener">
-									<img class="mx-auto" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/youtube.png" alt="Youtube">
-								</a>
-							<?php endif; ?>
-							<?php if ($spotify = get_field('social_media_spotify', 'option')) : ?>
-								<a class="social-platform-icon" href="<?php echo esc_url($spotify); ?>" target="_blank" rel="noopener">
-									<img class="mx-auto" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/spotify.png" alt="Youtube">
-								</a>
-							<?php endif; ?>
-						</div>
-					</div>
-			</div>
-			<div class="pt-[20px] pb-[30px] border-t border-t-[#D1D5DC]">
-				<p class="text-[14px] font-medium"><?php echo get_field('footer_copyright', 'option'); ?></p>
-			</div>
-		</div>
-	</footer>
+          <?php if ($tiktok = get_field('social_media_tiktok_link', 'option')) : ?>
+          <a href="<?php echo esc_url($tiktok); ?>" target="_blank" rel="noopener">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/social-icon-3.svg" alt="TikTok">
+          </a>
+          <?php endif; ?>
+        </div>
+
+        <div class="platform-links">
+          <?php if ($youtube = get_field('social_media_youtube', 'option')) : ?>
+          <a class="social-platform-icon" href="<?php echo esc_url($youtube); ?>" target="_blank" rel="noopener">
+            <img class="mx-auto" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/youtube.png"
+              alt="Youtube">
+          </a>
+          <?php endif; ?>
+          <?php if ($spotify = get_field('social_media_spotify', 'option')) : ?>
+          <a class="social-platform-icon" href="<?php echo esc_url($spotify); ?>" target="_blank" rel="noopener">
+            <img class="mx-auto" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/spotify.png"
+              alt="Youtube">
+          </a>
+          <?php endif; ?>
+        </div>
+      </div>
+    </div>
+    <div class="pt-[20px] pb-[30px] border-t border-t-[#D1D5DC]">
+      <p class="text-[14px] font-medium"><?php echo get_field('footer_copyright', 'option'); ?></p>
+    </div>
+  </div>
+</footer>
 </div>
 
 <script src="/wp-content/themes/the-bell/assets/js/hubspot.js"></script>
+<!-- Start of HubSpot Embed Code -->
+<script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/48534028.js"></script>
+<!-- End of HubSpot Embed Code -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
